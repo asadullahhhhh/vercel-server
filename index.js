d3ef4a133f99385367920b8e40edee80d49f0273
Asadullah <assadulla0172@gmail.com>
2025-05-23 03:21:48 +0600

    add the mongodb database setup

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+  // hobbyDB
+  // NYjXAEEJK6PDJEOC
+// mongoDB uri.
+const uri =
+  "mongodb+srv://hobbyDB:NYjXAEEJK6PDJEOC@cluster1.1w25q2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+
+// mongo client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+    try{
+        await client.connect()
+        await client.db("admin").command({ping : 1})
+        console.log(
+          "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+
+       
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
   res.send("Server running successfully");
