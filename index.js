247c6e81360e86b1726a9799ec087f64d7a57b62
Asadullah <assadulla0172@gmail.com>
2025-05-23 03:27:20 +0600

    add the user/:id get method

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-  // hobbyDB
-  // NYjXAEEJK6PDJEOC
+// hobbyDB
+// NYjXAEEJK6PDJEOC
 // mongoDB uri.
 const uri =
   "mongodb+srv://hobbyDB:NYjXAEEJK6PDJEOC@cluster1.1w25q2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
@@ -23,42 +23,42 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try{
-      await client.connect();
-      await client.db("admin").command({ ping: 1 });
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-      // mongoDB collections
-      const bannerCollection = client.db("hobbyDB").collection("hobbyBanner");
-      const groupsCollection = client.db("hobbyDB").collection("groupsDB");
-      const usersCollections = client.db("hobbyDB").collection("userDB");
+    // mongoDB collections
+    const bannerCollection = client.db("hobbyDB").collection("hobbyBanner");
+    const groupsCollection = client.db("hobbyDB").collection("groupsDB");
+    const usersCollections = client.db("hobbyDB").collection("userDB");
 
-      app.get("/banners", async (req, res) => {
-        const cursor = bannerCollection.find();
-        const banners = await cursor.toArray();
-        res.send(banners);
-      });
+    app.get("/banners", async (req, res) => {
+      const cursor = bannerCollection.find();
+      const banners = await cursor.toArray();
+      res.send(banners);
+    });
 
-      app.get("/groups", async (req, res) => {
-        const pointer = groupsCollection.find();
-        const groupData = await pointer.toArray();
-        res.send(groupData);
-      });
+    app.get("/groups", async (req, res) => {
+      const pointer = groupsCollection.find();
+      const groupData = await pointer.toArray();
+      res.send(groupData);
+    });
 
-      app.get("/groups/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await groupsCollection.findOne(query);
-        res.send(result);
-      });
+    app.get("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await groupsCollection.findOne(query);
+      res.send(result);
+    });
 
-      app.post("/groups", async (req, res) => {
-        const data = req.body;
-        const result = await groupsCollection.insertOne(data);
-        res.send(result);
-      });
+    app.post("/groups", async (req, res) => {
+      const data = req.body;
+      const result = await groupsCollection.insertOne(data);
+      res.send(result);
+    });
 
     //   users collection
     app.get("/users", async (req, res) => {
@@ -66,13 +66,18 @@ async function run() {
       const usersData = await pointer.toArray();
       res.send(usersData);
     });
-    }
-    finally{
 
-    }
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollections.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server running successfully");
